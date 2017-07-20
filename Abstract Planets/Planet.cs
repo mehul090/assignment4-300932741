a6e96139ded2f1a539451b3beabf4749434527f4
mehul <mkhosla2@my.centennialcollege.ca>
2017-07-20 15:24:51 -0400

    created class planets

---
+++ Abstract Planets/Planet.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Mehul khosla
+ * Date: july 17,2017
+ * Description: Ths is the abstract Planet class
+ * Version:0.1 
+ * */
+
+namespace Abstract_Planets
+{
+    /// <summary>
+    /// this is abstract Planet class
+    /// </summary>
+    public abstract class Planet
+    {
+        //Private instance variable (fields)
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPlanet;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        //public properties
+        public double Diameter
+        {
+            get;
+
+        }
+
+        public double Mass
+        {
+            get;
+            
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+
+        public string Name
+        {
+            get;
+           
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPlanet;
+            }
+            set
+            {
+                this._orbitalPlanet = value;
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        public double RotationalPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        //constructor--------------------------------------
+        /// <summary>
+        /// this is the main constructor for Planet class.
+        /// it takes three parameters - name(string) , diameter(double) , mass(double)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        public Planet(string name, double diameter, double mass)
+        {
+            this.Name = name;
+            this.Diameter = diameter;
+            this.Mass = mass;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("{} {} {} ");
+        }
+
+
+    }
+}
